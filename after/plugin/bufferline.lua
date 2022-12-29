59e71c17fb940039de0737373229eaac420055de
denkk0 <xkrama06@vutbr.cz>
2022-12-29 12:12:25 +0100

    mappings for tabs

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -12,7 +12,21 @@ require("bufferline").setup{
         separator_style = "thick",
         enforce_regular_tabs = false,
         always_show_bufferline = true,
-        sort_by = "directory",
+        sort_by = "incremental_after_current",
     }
 }
+
+vim.keymap.set("n", "<leader>1", ":BufferLineGoToBuffer 1<CR>")
+vim.keymap.set("n", "<leader>2", ":BufferLineGoToBuffer 2<CR>")
+vim.keymap.set("n", "<leader>3", ":BufferLineGoToBuffer 3<CR>")
+vim.keymap.set("n", "<leader>4", ":BufferLineGoToBuffer 4<CR>")
+vim.keymap.set("n", "<leader>5", ":BufferLineGoToBuffer 5<CR>")
+vim.keymap.set("n", "<leader>6", ":BufferLineGoToBuffer 6<CR>")
+vim.keymap.set("n", "<leader>7", ":BufferLineGoToBuffer 7<CR>")
+vim.keymap.set("n", "<leader>8", ":BufferLineGoToBuffer 8<CR>")
+vim.keymap.set("n", "<leader>9", ":BufferLineGoToBuffer 9<CR>")
+vim.keymap.set("n", "<leader>0", ":BufferLineGoToBuffer 10<CR>")
+vim.keymap.set("n", "<leader>$", ":BufferLineGoToBuffer -1<CR>")
+vim.keymap.set("n", "<leader>]", ":BufferLineMoveNext<CR>")
+vim.keymap.set("n", "<leader>[", ":BufferLineMovePrev<CR>")
 

79fa35b0e862d533242d9d1060325b9eb14f0032
denkk0 <xkrama06@vutbr.cz>
2023-01-12 10:37:11 +0100

    variable size window

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -5,8 +5,12 @@ require('toggleterm').setup{
     direction = 'float',
     float_opts = {
         border = 'double',
-        width = 120,
-        height = 30,
+        width = function()
+            return math.floor(vim.o.columns * 0.8)
+        end,
+        height = function()
+            return math.floor(vim.o.lines * 0.8)
+        end,
         winblend = 10,
     }
 }

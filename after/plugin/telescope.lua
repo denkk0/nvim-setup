8757e5a5ba0782cefc6429469070bae5eacd9209
denkk0 <xkrama06@vutbr.cz>
2023-07-03 21:47:45 +0200

    custimze find file telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,14 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+
+local function find_files()
+    return builtin.find_files({
+        file_ignore_patterns = { "node_modules/", ".venv/", ".git/" },
+        hidden = true,
+        no_ignore = true
+    })
+end
+
+vim.keymap.set('n', '<leader>pf', find_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>pl', builtin.resume, {})
 
